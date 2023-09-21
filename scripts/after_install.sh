982adea4db0029ab136f89f17c84f82df5330dc9
Gourish Pisal <62263679+gourishpisal@users.noreply.github.com>
2023-09-21 15:55:47 -0400

    Add files via upload

---
+++ scripts/after_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# scripts/after_install.sh
+
+# Install project dependencies
+cd /home/ec2-user/CML-Backend-main
+npm install --save
---
+++ scripts/application_start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# scripts/application_start.sh
+
+# Start the application using nodemon
+cd /home/ec2-user/CML-Backend-main
+npm install -g nodemon
+nodemon app

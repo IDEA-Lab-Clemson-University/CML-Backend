42d3272f553a3c766362c16e3cfbe7806df592f5
Gourish Pisal <62263679+gourishpisal@users.noreply.github.com>
2023-09-21 15:54:14 -0400

    Delete scripts directory

--- scripts/application_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# scripts/application_start.sh
-
-# Start the application using nodemon
-cd /home/ec2-user/CML-Backend
-npm install -g nodemon
-nodemon app
--- scripts/before_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# scripts/before_install.sh
-
-# Install Node.js v16
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-# Install npm
-sudo npm install -g npm

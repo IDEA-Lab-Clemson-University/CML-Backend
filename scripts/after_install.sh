2303d35bb74a4eef3ed782d0b8daff1ca80bd495
Gourish Pisal <62263679+gourishpisal@users.noreply.github.com>
2023-09-21 22:44:51 -0400

    Delete scripts directory

--- scripts/after_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# scripts/after_install.sh
-
-# Install project dependencies
-echo "install any existing node servers"
--- scripts/application_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# scripts/application_start.sh
-
-# Start the application using nodemon
-echo "start any existing node servers"

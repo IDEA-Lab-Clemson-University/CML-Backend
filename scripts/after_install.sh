551a85e7cf4f75b5375674ab209f14a893a64f28
Gourish Pisal <62263679+gourishpisal@users.noreply.github.com>
2023-09-21 23:02:48 -0400

    Add files via upload

---
+++ scripts/after_install.sh
@@ -0,0 +1,15 @@
+cd /home/ec2-user/CML-Backend-main
+
+# Install Node.js dependencies using npm
+npm install
+
+# Perform any other post-installation tasks if needed
+# For example, you can run additional setup or configuration scripts here
+# ./setup.sh
+
+# Ensure that the necessary permissions are set for your application files
+# chown -R ec2-user:ec2-user /home/ec2-user/CML-Backend-main
+
+# Optionally, you can restart services or perform other actions here
+
+exit 0

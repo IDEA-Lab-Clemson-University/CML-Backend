0f84b1d4ce42a199a32235bbd80e423c3e207a35
Deepika Sistla <dsistla@g.clemson.edu>
2023-09-29 04:13:32 -0700

    Server update

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect("mongodb://localhost:27017/spot_agency", 
+mongoose.connect("mongodb+srv://idealab:Clem%240n%21de%40@cluster0.4t0cj04.mongodb.net/spotagency_5", 
+// mongoose.connect("mongodb://localhost:27017/spot_agency"
 //mongoose.connect(process.env.MONGO_DB_URL, 
 { useNewUrlParser: true, useUnifiedTopology: true}).then(()=> {
     console.log('connected to mongodb database successfully.');

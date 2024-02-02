349f7ca89ccbde7b862e84b7a26a41785e01a1ea
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2024-02-02 15:34:54 -0500

    cors-changed

--- app.js
+++ app.js
@@ -53,6 +53,22 @@ expressJSDocSwagger(app)(options);
 
 //middlewares
 
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	if (req.method === "OPTIONS") {
+		res.header(
+			"Access-Control-Allow-Methods",
+			"GET, POST, PUT, PATCH, DELETE"
+		);
+		return res.status(200).json({});
+	}
+	next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
